311b3ac119d7f11ebdcae13c196f2a4a84c1c09b
Sarah.H <129738346+SarahHAUTOT@users.noreply.github.com>
2024-11-20 13:53:50 +0100

    Create Q4.java

---
+++ Q4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Q3
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Tappez un chiffre : ");
+
+		int nb = sc.nextInt();
+		for (int i=1; i<=nb; i++) 
+		{
+			for (int y = 1; y <= i; y++)
+				System.out.print("*");
+
+			System.out.println();
+		}
+	}
+}
